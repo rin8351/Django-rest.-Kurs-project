a884750a8109065acf1df0c7c9c444c3ffa12237
rin8351 <57259194+rin8351@users.noreply.github.com>
2022-07-02 11:28:19 +0300

    Update views.py

--- Test_django-to-postgresql/post/postt/views.py
+++ Test_django-to-postgresql/post/postt/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
 from .models import *
 from .forms import *
 from django.urls import reverse
-from django.views.generic import UpdateView, DeleteView
+from django.views.generic import UpdateView, DeleteView, CreateView
 
 def kurs_f(request):
-    kurs_p=kurs.objects.all()
-    form=AddkursForm(request.POST)
-    if request.method == 'POST':
-        form = AddkursForm(request.POST)
-        if form.is_valid():
-            form.save()
-            form = AddkursForm()
-            return HttpResponseRedirect(reverse('kurs'))
-    context={
-        'kurs_p':kurs_p,
-        'form':form,
-    }
-    return render(request, "postt/postt.html", context=context)
+    if request.user.is_authenticated:
+        kurs_p=kurs.objects.all()
+        form=AddkursForm(request.POST)
+        if request.method == 'POST':
+            form = AddkursForm(request.POST)
+            if form.is_valid():
+                form.save()
+                form = AddkursForm()
+                return HttpResponseRedirect(reverse('kurs'))
+        context={
+            'kurs_p':kurs_p,
+            'form':form,
+        }
+        return render(request, "postt/postt.html", context=context)
+    else:
+        return HttpResponseRedirect(reverse('login'))
 
 class update_row(UpdateView):
     model = kurs
@@ -38,3 +44,21 @@ class delete_row(DeleteView):
             return HttpResponseRedirect(reverse('kurs'))
         else:
             return super(delete_row, self).post(request, *args, **kwargs)
+
+class Login(LoginView):
+    form = AuthenticationForm
+    template_name = 'postt/login.html'
+
+    def get_success_url(self):
+        return reverse('kurs')
+
+    def get_context_data(self, **kwargs):
+        context = super(Login, self).get_context_data(**kwargs)        
+        return context
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound("<h1>Page not found</h1>")
