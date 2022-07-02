8170600639acd3a4325f821602eba0daa43b450a
rin8351 <57259194+rin8351@users.noreply.github.com>
2022-07-02 11:29:00 +0300

    Update urls.py

--- Test_django-to-postgresql/post/post/urls.py
+++ Test_django-to-postgresql/post/post/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from postt.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('postt.urls')),
 ]
+
+handler404=pageNotFound
