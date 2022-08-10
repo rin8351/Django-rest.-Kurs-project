8620eccb80ed3dd926b4886daa1d206fd88c31fb
rin8351 <57259194+rin8351@users.noreply.github.com>
2022-08-11 00:49:06 +0300

    Delete Test_django-to-postgresql directory

--- Test_django-to-postgresql/post/postt/templatetags/postt_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from postt.models import *
-
-register=template.Library()
-
-@register.inclusion_tag('postt/script_for_sort_table.html')
-def script_for_sort_table():
-    return {}
